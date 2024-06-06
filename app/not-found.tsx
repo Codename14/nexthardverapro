e5c8096ad11b0a80596b22a4b2a447a5ce947b80
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-06-06 21:56:39 +0200

    remove translation

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+export default function NotFoundPage() {
+    return (
+        <html>
+            <body className='light-theme'>
+                <section className='page_404 under-navbar'>
+                    <div className='contant_box_404'>
+                        <div className='four_zero_four_bg'>
+                            <h1 className='title-404'>404</h1>
+                        </div>
+                        <h3 className='section-title'>Úgy tűnik elvesztél,</h3>
+                        <div className='flex'>
+                            <p>az oldal amit keresel nem található!</p>
+                        </div>
+                        <Link href='/' className='btn btn--secondary'>
+                            Vissza a főoldalra
+                        </Link>
+                    </div>
+                </section>
+            </body>
+        </html>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-    return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <p>alma</p>
-        </main>
-    );
+    // const t = useTranslations('Navigation');
+
+    return <main className='flex min-h-screen flex-col items-center justify-between p-24'>{/* <p>{t('home')}</p> */}</main>;
 }
