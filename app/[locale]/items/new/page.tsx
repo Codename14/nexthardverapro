9635df6856d2ba12192a8eff9c2588d4bcae4e05
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-06-18 20:05:30 +0200

    update

--- app/[locale]/items/new/page.tsx
+++ app/[locale]/items/new/page.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
 export default function Page() {
-    return <div>New product sell form</div>;
+    return (
+        <>
+            <section>
+                <h1 className='section-title'>Termék eladása</h1>
+                <form action='' className='flex flex-col'>
+                    <label htmlFor=''>Hirdetés címe</label>
+                    <label htmlFor=''>Képek</label>
+                    <input className='input--primary' type='file' />
+                    <input className='input--primary' type='text' placeholder='product name' />
+                    <label htmlFor=''>Ára</label>
+                    <input className='input--primary' type='number' placeholder='product price' />
+                    <label htmlFor=''>Adj leírást a termékedhez</label>
+                    <textarea cols={50} className='input--primary' placeholder='product description' />
+
+                    <label htmlFor=''>Kategória</label>
+                    <select name='Kategória' id=''>
+                        <option value=''>AAA</option>
+                        <option value=''>BBB</option>
+                        <option value=''>CCC</option>
+                    </select>
+                    <label htmlFor=''>Hely</label>
+                    <input className='input--primary' type='text' placeholder='hely' />
+                    <button className='btn btn--primary'>Submit</button>
+                </form>
+            </section>
+        </>
+    );
 }
