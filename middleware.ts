cc480b89c5460daeb538464f11c0be771c3702e7
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-06-19 13:51:59 +0200

    trying rebuild with new env variables

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: ['/', '/(hu|en|de)/:path*'],
-    // matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+    //// matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 };
