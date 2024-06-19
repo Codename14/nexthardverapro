4acf4e8df6d82691ad661970e406cdf151e17dea
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-06-19 13:11:49 +0200

    build profile page and message page

--- app/[locale]/(private)/account/page.tsx
+++ app/[locale]/(private)/account/page.tsx
@@ -1,6 +1,8 @@
 import { readUserData } from '@/lib/actions';
 import { createSerClient } from '@/lib/supabase/server';
-
+import { DEFAULT_IMG } from '@/lib/constants';
+import Image from 'next/image';
+import { FaLocationDot, FaMedal, FaPlaceOfWorship, FaVoicemail, FaWifi } from 'react-icons/fa6';
 export default async function AccountPage() {
     const user = await readUserData();
 
@@ -9,9 +11,44 @@ export default async function AccountPage() {
     }
     console.log('user', user);
     return (
-        <div className='my-10'>
-            <p>{user.email}</p>
+        <div className='under-navbar screen-container'>
+            <section className='own-profile'>
+                <div className='flex gap-5'>
+                    <div className='profile__img relative size-10'>
+                        <Image src={DEFAULT_IMG} alt={'asd'} fill />
+                    </div>
+                    <div className='profile__details'>
+                        <div className='profile__title'>
+                            <h4 className='small-title'>Nagy Adam</h4>
+                            <p>Még nincs értékelés</p>
+                        </div>
+                        <div className='profile__info'>
+                            <div className='flex'>
+                                <FaVoicemail size={25} />
+                                <p>{user.email}</p>
+                            </div>
+                            <div className='flex'>
+                                <FaLocationDot size={25} />
+                                <p>Hungary</p>
+                            </div>
+                            <div className='flex'>
+                                <FaWifi size={25} />
+                                <p>0 követő , 0 követés</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <button className='btn btn--primary'>Profil szerkesztése</button>
+            </section>
             <p>{user.last_sign_in_at}</p>
+
+            <section>
+                <div className='flex gap-2 action'>
+                    <button className='btn btn--primary'>Termékek</button>
+                    <button className='btn btn--primary'>Értékelések</button>
+                </div>
+            </section>
         </div>
     );
 }
--- app/[locale]/(private)/messages/MessageDetails.tsx
+++ app/[locale]/(private)/messages/MessageDetails.tsx
@@ -1,29 +1,75 @@
 'use client';
-import EmojiPicker from 'emoji-picker-react';
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { FaIcons, FaImage } from 'react-icons/fa6';
+import { DEFAULT_IMG } from '@/lib/constants';
+import Image from 'next/image';
 
 export default function MessageDetails() {
     const [isEmojiOpen, setIsEmojiOpen] = useState(false);
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        watch,
+        setValue,
+        setError,
+    } = useForm({});
+    const messageValue = watch('message');
+
+    const handleArrowKeys = (event: React.KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            setIsEmojiOpen(false);
+        }
+    };
+    const handleEmoji = (emoji: EmojiClickData) => {
+        console.log(emoji);
+        setValue('message', `${messageValue}${emoji.emoji}`);
+    };
+
     return (
         <>
             <div className='message-wrapper'>
                 <div className='messages-container'>
-                    <div className='message-sender'>Nagy Ali</div>
-                    <div className='message-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ab debitis neque molestias, cum corporis recusandae
-                        perspiciatis autem voluptates itaque.
+                    <div className='message'>
+                        <div className='flex'>
+                            <div className='user-img'>
+                                <Image src={DEFAULT_IMG} alt={'aaa'} fill />
+                            </div>
+                            <p className='message-text'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ab debitis neque molestias, cum corporis recusandae
+                                perspiciatis autem voluptates itaque.
+                            </p>
+                        </div>
+                        <p>5 min ago</p>
                     </div>
                 </div>
                 <form className='message-action'>
                     <FaImage size={25} />
-                    <input className='input--primary' type='text' />
+                    <input
+                        {...register('message', {
+                            required: 'Message is required',
+                        })}
+                        onKeyDown={(event) => {
+                            handleArrowKeys(event);
+                        }}
+                        onFocus={() => setIsEmojiOpen(false)}
+                        className='input--primary'
+                    />
                     <button type='button' onClick={() => setIsEmojiOpen((prev) => !prev)} className='btn--emoji'>
                         <FaIcons size={25} />
                     </button>
                     {/* {isEmojiOpen && ( */}
-                    <div className='emoji-picker'>
-                        <EmojiPicker open={isEmojiOpen} />
+
+                    <div
+                        className='emoji-picker'
+                        onKeyDown={(event) => {
+                            handleArrowKeys(event);
+                        }}
+                    >
+                        <EmojiPicker open={isEmojiOpen} onEmojiClick={handleEmoji} />
                     </div>
                     {/* )} */}
                     <button className='btn btn--primary'>Küldés</button>
