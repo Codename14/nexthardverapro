ecaffb71896b5880bd71e61a4e6b2502a481f52f
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-07-02 11:06:23 +0200

    fix: replace header profile popover to menu

--- components/header/ProfileMenu.tsx
+++ components/header/ProfileMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Popover, PopoverButton, PopoverPanel, useClose } from '@headlessui/react';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import Image from 'next/image';
 import { DEFAULT_IMG } from '@/lib/constants';
 import { Link } from '@/navigation';
@@ -11,30 +11,35 @@ import { CiLogout, CiSettings, CiUser } from 'react-icons/ci';
 export default function ProfileMenu({ userImg }: { userImg?: string }) {
     const t = useTranslations('Navigation');
 
-    let close = useClose();
     return (
-        <>
-            <Popover className='popover'>
-                <PopoverButton className={'popover-button'}>
+        <div className='header-menu'>
+            <Menu>
+                <MenuButton>
                     <div className='profile_img '>
                         <Image src={userImg ?? DEFAULT_IMG} alt={'profile'} fill className='rounded-xl w-full h-full object-cover' />
                     </div>
-                </PopoverButton>
-                <PopoverPanel anchor='bottom' className='popover-list'>
-                    <Link onClick={() => close()} href='/account' className='flex gap-2'>
-                        <CiUser size={25} className='icon' />
-                        {t('profile')}
-                    </Link>
-                    <Link onClick={() => close()} href='/user-settings' className='flex gap-2'>
-                        <CiSettings size={25} className='icon' />
-                        {t('settings')}
-                    </Link>
-                    <Link onClick={() => close()} href='/' className='flex gap-2'>
-                        <CiLogout size={25} className='icon' />
-                        {t('logout')}
-                    </Link>
-                </PopoverPanel>
-            </Popover>
-        </>
+                </MenuButton>
+                <MenuItems anchor='bottom' className='menu-list'>
+                    <MenuItem>
+                        <Link href='/account' className='flex gap-2'>
+                            <CiUser size={25} className='icon' />
+                            {t('profile')}
+                        </Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link href='/user-settings' className='flex gap-2'>
+                            <CiSettings size={25} className='icon' />
+                            {t('settings')}
+                        </Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link href='/' className='flex gap-2'>
+                            <CiLogout size={25} className='icon' />
+                            {t('logout')}
+                        </Link>
+                    </MenuItem>
+                </MenuItems>
+            </Menu>
+        </div>
     );
 }
