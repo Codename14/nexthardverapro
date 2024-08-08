556c64d17b861613309ab6afaa777621267698cd
Alex Banai <46088872+Codename14@users.noreply.github.com>
2024-08-08 09:30:40 +0200

    fix: new product form disable access if has no user

--- app/[locale]/items/new/page.tsx
+++ app/[locale]/items/new/page.tsx
@@ -1,9 +1,16 @@
 import prisma from '@/lib/pismaDB';
 import React from 'react';
 import ManageProductForm from './ManageProductForm';
+import { readUserData } from '@/lib/actions';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
     const categories = await prisma.categories.findMany();
+
+    const user = await readUserData();
+    if (!user) {
+        redirect('/');
+    }
 
     return (
         <>
